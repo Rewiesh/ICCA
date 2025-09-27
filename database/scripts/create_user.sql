213baba74a065254c3f4e8e8f941a7cdaaf4998b
sameerkewal <sameerkewal1@gmail.com>
2025-09-27 16:13:00 -0300

    cleaned up scripts for initial deployment + added create user script

--- database/scripts/create_user.sql
+++ database/scripts/create_user.sql
@@ -27,6 +27,10 @@ grant create trigger to icca;
 grant create type to icca;
 grant create view to icca;
 grant execute dynamic mle to icca;
+grant create any context to icca;
+
+
+alter user icca quota unlimited on icca_ts;
 
 
 
